222611c3b3482fcae6d81e52610c42f09bcd3571
Sam Bruno <sjbrunster@gmail.com>
2016-06-01 03:30:25 +0000

    Create article new and create views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
-    belongs_to :user
-    validates :title, presence: true, length: { minimum:3, maximum: 50 }
-    validates :description, presence: true, length: { minimum:10, maximum: 300 }
-    #validates :user_id, presence: true
+  belongs_to :user
+  # validates :title, presence: true, length: { minimum:3, maximum: 50 }
+  # validates :key, presence: true, length: { minimum:1, maximum: 5 }
+  # validates :instrument, presence: true, length: { minimum:2, maximum: 30 }
+  # validates :description, presence: true, length: { minimum:10, maximum: 300 }
+  # validates :user_id, presence: true
 end
