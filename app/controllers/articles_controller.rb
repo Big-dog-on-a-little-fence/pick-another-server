b92c546803ebfa22e6f780887859e6109096b080
Sam Bruno <sjbrunster@gmail.com>
2016-06-01 19:21:31 +0000

    Add user id to articles, create article partial, modify views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
+    @articles = Article.all
   end
 
   def new
@@ -10,15 +13,29 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:success] = "Article was successfully created."
+      redirect_to article_path(@article)
+    else
+      render 'new'  ## render new article template in case of failure for another try
+    end
   end
 
   def show
   end
+  
+  def destroy
+  end
 
   private
+    def set_article  ## create private method to reuse
+      @article = Article.find(params[:id])
+    end
     def article_params  # whitelist method
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :key, :instrument, :description)
     end
 
 end
+
