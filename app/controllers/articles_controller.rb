49032ff2e34554745a6a307841b19004abd07131
Sam Bruno <sjbrunster@gmail.com>
2016-06-02 14:25:57 +0000

    Complete articles controller methods and article edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,9 +28,18 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'  ## render edit article template in case of failure for another try
+    end
   end
 
   def destroy
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted."
+    redirect_to articles_path
   end
 
   private
