8e7f65f2a588a10487de70d29ee2bc09b4dee95a
Sam Bruno <sjbrunster@gmail.com>
2016-07-06 15:00:16 +0000

    Add tunes model validations

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -1,6 +1,6 @@
 class Tune < ApplicationRecord
   has_many :repertoires
   has_many :users, through: :repertoires
-  validates :name, presence: true, length: { minimum:3, maximum: 50 }
+  validates :name, presence: true, :uniqueness => {:case_sensitive => false}, length: { minimum:3, maximum: 50 }
   validates :key, presence: true, length: { minimum:1, maximum: 5 }
 end
