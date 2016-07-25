e41d76029b1a858d7ec933fd35a85a78fbfa6857
Sam Bruno <sjbrunster@gmail.com>
2016-07-25 22:44:40 +0000

    Fix resource display on tune views

--- app/helpers/tunes_helper.rb
+++ app/helpers/tunes_helper.rb
@@ -3,12 +3,12 @@ module TunesHelper
   def embed_youtube(youtube_url)
     ## http://blog.41studio.com/embed-responsive-youtube-video-in-rails-4/
     youtube_id = youtube_url.split("/").last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", allowfullscreen: "allowfullscreen")
+    content_tag(:div, content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", allowfullscreen: "allowfullscreen"), class: "embed-container")
   end
   
   def embed_spotify(spotify_url)
     spotify_id = spotify_url.split("%3A").last
-    content_tag(:iframe, nil, src: "https://embed.spotify.com/?uri=spotify%3Atrack%3A#{spotify_id}")
+    content_tag(:div, content_tag(:iframe, nil, src: "https://embed.spotify.com/?uri=spotify%3Atrack%3A#{spotify_id}"), class: "embed-container")
   end
 
   def embed(url)
@@ -18,7 +18,7 @@ module TunesHelper
     when url.include?('open.spotify')
       embed_spotify(url)
     else
-      link_to(url, url, target: "_blank")
+      content_tag(:div, link_to(truncate(url, length:40), url, target: "_blank"))
     end
   end
 
