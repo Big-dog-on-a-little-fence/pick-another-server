b4b649ab50baf488917159cfa7cbeba57108cca7
Sam Bruno <sjbrunster@gmail.com>
2016-07-26 16:33:33 +0000

    Fix tunes sorting by genre

--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -7,6 +7,7 @@ class TunesController < ApplicationController
     @q = Tune.ransack(params[:q])
     @q.sorts = 'updated_at desc' if @q.sorts.empty?
     @tunes = @q.result(distinct: true).includes(:genres).page(params[:page]).per(100)
+    #@tunes = @q.result.includes(:genres).page(params[:page]).per(100)
   end
   
   def show
