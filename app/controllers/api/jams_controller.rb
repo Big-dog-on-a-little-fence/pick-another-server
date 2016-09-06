49423e88838d357194448199e3b3d7c4e8b82207
Sammy Nave <sammy.nave@gmail.com>
2016-09-05 23:34:41 -0400

    basic create jam api

--- app/controllers/api/jams_controller.rb
+++ app/controllers/api/jams_controller.rb
@@ -4,5 +4,20 @@ module Api
       jam = Jam.find(params[:id])
       render json: jam, include: ['users']
     end
+
+    def create
+      jam = Jam.new(request_params)
+      if jam.save
+        render json: jam, status: :created
+      else
+        render json: jam, serializer: ActiveModel::Serializer::ErrorSerializer, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def request_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+    end
   end
 end
