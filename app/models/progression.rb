6e25bba87898069ebdd789b0614060fd1a3f6934
Sam Bruno <sjbrunster@gmail.com>
2017-08-22 21:11:56 +0000

    measures order created_at asc

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -1,6 +1,6 @@
 class Progression < ApplicationRecord
   belongs_to :chart
-  has_many :measures
+  has_many :measures, -> { order(created_at: :asc) }
   has_many :chords
   
   accepts_nested_attributes_for :measures, allow_destroy: true
