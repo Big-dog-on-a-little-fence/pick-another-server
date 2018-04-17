0f96fc02fe2cee98a73b51afcaa0b0d40ea542c5
Sam Bruno <sjbrunster@gmail.com>
2018-04-17 15:29:57 +0000

    add testing

--- test/controllers/genres_controller_test.rb
+++ test/controllers/genres_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class GenresControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  include Devise::Test::IntegrationHelpers
+  setup do
+    sign_in users(:alice)
+    @genre = genres(:bluegrass)
+  end
+
+  test "should get index" do
+    get genres_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_genre_url
+    assert_response :success
+  end
+
+  test "should create genre" do
+    assert_difference('Genre.count') do
+      post genres_url, params: { genre: { name: "Choro" } }
+    end
+
+    assert_redirected_to genres_url
+  end
+
+  test "should show genre" do
+    get genre_url(@genre)
+    assert_response :success
+  end
+
+  # test "should get edit" do
+  #   get edit_genre_url(@genre)
+  #   assert_response :success
   # end
+
+  # test "should update genre" do
+  #   patch genre_url(@genre), params: { genre: { name: "Not What It Was" } }
+  #   assert_redirected_to genre_url(@genre)
+  # end
+
+  # test "should destroy genre" do
+  #   assert_difference('Genre.count', -1) do
+  #     delete genre_url(@genre)
+  #   end
+
+  #   assert_redirected_to genres_url
+  # end
+  
+  teardown do
+    sign_out :user
+  end
 end
