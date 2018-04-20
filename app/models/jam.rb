d7442873edd4d512c76af880428c3118dd02499a
Sam Bruno <sjbrunster@gmail.com>
2018-04-20 12:53:32 +0000

    add jam index, comments to jams

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -2,5 +2,7 @@ class Jam < ApplicationRecord
   has_many :jam_users
   has_many :users, through: :jam_users
   has_and_belongs_to_many :tunes
+  has_many :comments, as: :commentable
+  
   ## TODO: validations
 end
