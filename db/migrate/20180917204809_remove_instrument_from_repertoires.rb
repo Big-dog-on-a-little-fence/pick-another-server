3492023454999961aee2bb9cf3bcbf487f8df5db
Sam Bruno <sjbrunster@gmail.com>
2018-09-25 15:43:52 +0000

    clean up migrations

--- db/migrate/20180917204809_remove_instrument_from_repertoires.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInstrumentFromRepertoires < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :repertoires, :instrument, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917204809) do
+ActiveRecord::Schema.define(version: 20180925154100) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "trackable_type"
