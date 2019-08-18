6df676ac20c033c456eb8c46d8469354e7eb3a0b
Sam Bruno <sjbrunster@gmail.com>
2019-08-18 13:56:57 +0000

    fix instrument tune field for activity search

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,6 +3,7 @@ class ActivitiesController < ApplicationController
     activities_includes = [:owner, :trackable]
     @q = PublicActivity::Activity.ransack(params[:q])
     @activities = @q.result.includes(activities_includes).order("created_at desc").page(params[:page]).per(50)
-    @activity_types = {"Tune":"Tune", "InstrumentTune":"Learned Tune", "Comment":"Comment"}
+    # @activity_types = {"Tune":"Tune", "InstrumentTune":"Learned Tune", "Comment":"Comment"}
+    @activity_types = ["Tune", "InstrumentTune", "Comment"]
   end
 end
